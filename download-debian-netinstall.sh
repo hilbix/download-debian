c92e42e268e7e2f9763a72dcae148263bd416f5a
Valentin Hilbig <externer.dl.hilbig@muenchen.de>
2018-01-17 15:21:54 +0100

    Support download of Ubuntu brands

--- download-debian-netinstall.sh
+++ download-debian-netinstall.sh
@@ -1,31 +1,54 @@
 #!/bin/bash
 
-BASE="http://cdimage.debian.org/debian-cd/%s/amd64/iso-cd/debian-%s-amd64-netinst.iso"
 VERS="$1"
-[ -n "$VERS" ] || read -p 'Version (like: 9.3.0): ' VERS || exit
 
 OOPS() { echo "$*" >&2; exit 23; }
 o() { "$@" || OOPS "exec $?: $*"; }
 v() { local -n __var__="$1"; __var__="$("${@:2}")" || OOPS "exec $?: $*"; }
 
-case "$VERS" in
-*[^0-9.]*)	OOPS "Huh?";;
-[0-9]*.*[0-9])	;;
-*)		OOPS "Huh?";;
+[ -n "$VERS" ] || read -p 'Version (like: 9.3.0): ' VERS || exit
+
+BRAND="$VERS"
+VERS="${VERS#[a-z]*-}"
+BRAND="${BRAND%"$VERS"}"
+BRAND="${BRAND%-}"
+case "$BRAND" in
+(''|debian)		SIGS=sign
+			BRAND=debian
+			BASE="http://cdimage.debian.org/debian-cd/%s/amd64/iso-cd/debian-%s-amd64-netinst.iso"
+			SHA512=y
+			KEYS=/usr/share/keyrings/debian-role-keys.gpg
+			;;
+(*ubuntu)		SIGS=gpg
+			BASE="http://cdimage.ubuntu.com/$BRAND/releases/%s/release/$BRAND-%s-desktop-amd64.iso"
+			SHA512=n
+			KEYS=/usr/share/keyrings/ubuntu-archive-keyring.gpg
+			;;
+(*)			OOPS "unknown brand: $BRAND for version $VERS";;
 esac
+
+case "$VERS" in
+*[^0-9.]*)	OOPS "Huh? Version is $VERS";;
+[0-9]*.*[0-9])	;;
+*)		OOPS "Huh? Version is $VERS";;
+esac
+
+[ -f "$KEYS" ] || OOPS missing file: "$KEYS"
 
 o printf -v URL "$BASE" "$VERS" "$VERS"
 
-[ -d "$VERS" ] || o mkdir "$VERS"
-o cd "$VERS"
+[ -d "$BRAND-$VERS" ] || o mkdir "$BRAND-$VERS"
+o cd "$BRAND-$VERS"
 
 SUB="${URL%/*}"
 DAT="${URL##*/}"
-SUMS=(MD5SUMS SHA1SUMS SHA256SUMS SHA512SUMS)
+SUMS=(MD5SUMS SHA1SUMS SHA256SUMS)
+[ n = "$SHA512" ] || SUMS+=(SHA512SUMS)
 
+[ -s ~/.prox ] &&
 . ~/.prox
 
-for a in "$DAT" "${SUMS[@]}" "${SUMS[@]/%/.sign}"
+for a in "$DAT" "${SUMS[@]}" "${SUMS[@]/%/.$SIGS}"
 do
 	[ -s "$a" ] && continue
 	wget -- "$SUB/$a"
@@ -36,7 +59,7 @@ denazify()
 {
 local murx
 
-murx="$(LC_ALL=C.UTF-8 "$@" 2>&1)" || return
+murx="$(LC_ALL=C.UTF-8 "$@" 2>&1)" || { local e=$?; echo "$murx"; return $e; }
 
 echo "$murx" |
 
@@ -56,14 +79,18 @@ grep -v '^gpg: WARNING: This key is not certified with a trusted signature!' |
 grep -v '^gpg:          There is no indication that the signature belongs to the owner.' |
 
 # Nope, I really cannot stand anymore to hear you talking about right and order!
-grep -v '^Primary key fingerprint: [0-9A-F ]*$'
+grep -v '^Primary key fingerprint: [0-9A-F ]*$' |
+
+# Instead, we want to live long and prosper
+grep '[^[:space:]]'
 }
 
 # See https://stackoverflow.com/a/35820272
 for a in "${SUMS[@]}"
 do
 	printf '%12s: ' "$a"
-	o denazify gpg --no-default-keyring --keyring /usr/share/keyrings/debian-role-keys.gpg --verify "$a.sign"
+	[ sign = "$SIGS" ] || [ -L "$a.sign" ] || o ln -s "$a.$SIGS" "$a.sign"
+	o denazify gpg --no-default-keyring --keyring "$KEYS" --verify "$a.sign"
 done
 
 check()
@@ -79,4 +106,6 @@ OOPS "$1 fail"
 check MD5SUMS		md5sum
 check SHA1SUMS		sha1sum
 check SHA256SUMS	sha256sum
+[ n = "$SHA512" ] ||
 check SHA512SUMS	sha512sum
+
